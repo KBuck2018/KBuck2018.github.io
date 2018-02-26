ddcd957c2a88725410a22a4a3f15f3d812555129
Kevin Buckley <kevin.buckley2013@gmail.com>
2018-02-26 11:46:12 -0500

    added portfolio page and fixed some issues with about page

---
+++ portfolio.js
@@ -0,0 +1,3 @@
+function myFunction(){
+        document.querySelector("#description").innerHTML = 'For this project, I was tasked with developing a web application using HTML/CSS and basic JavaScript. To do this effectively, I built out the entire application by using div elements and buttons in html/css and then creating a function for each. Naturally, this is as ineffective as you can get, so once I had built out my rudimentary layout, I refactored everything by automating my button creation, using key identifiers. This allowed me to effectively pin point each button and cut my code from 400 lines to 100. I plan on giving this application some new features soon including video content, sounds, a countdown, and multiplayer.'        
+}

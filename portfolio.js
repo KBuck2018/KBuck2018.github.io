4462cdcb7eaa47d0d16e8a3b020d4fd42c81e8dd
Kevin Buckley <kevin.buckley2013@gmail.com>
2018-04-03 08:45:55 -0400

    uodating portfolio

--- portfolio.js
+++ portfolio.js
@@ -1,3 +1,10 @@
-function myFunction(){
-        document.querySelector("#description").innerHTML = 'For this project, I was tasked with developing a web application using HTML/CSS and basic JavaScript. To do this effectively, I built out the entire application by using div elements and buttons in html/css and then creating a function for each. Naturally, this is as ineffective as you can get, so once I had built out my rudimentary layout, I refactored everything by automating my button creation, using key identifiers. This allowed me to effectively pin point each button and cut my code from 400 lines to 100. I plan on giving this application some new features soon including video content, sounds, a countdown, and multiplayer.'        
+function myFunction() {
+  document.querySelector("#description").innerHTML =
+    "For this project, I was tasked with developing a web application using HTML/CSS and basic JavaScript. To do this effectively, I built out the entire application by using div elements and buttons in html/css and then creating a function for each. Naturally, this is as ineffective as you can get, so once I had built out my rudimentary layout, I refactored everything by automating my button creation, using key identifiers. This allowed me to effectively pin point each button and cut my code from 400 lines to 100. I plan on giving this application some new features soon including video content, sounds, a countdown, and multiplayer.";
+}
+
+function myFunctionRecipe() {
+  document.querySelector(
+    "#description2"
+  ).innerHTML = `Please check out my youtube video describing the project. <a href="https://youtu.be/7AHmtj81FbQ">Youtube</a>`;
 }
